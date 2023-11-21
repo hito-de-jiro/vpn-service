89f9f6a86fd1eac87e886bb1ed68d92033db59b4
hito-de-jiro <lollexuss@gmail.com>
2023-11-21 15:29:17 +0200

    fetch: modified vpnservice/views.py, added interaction with javascript

--- vpnservice/views.py
+++ vpnservice/views.py
@@ -1,10 +1,7 @@
-import pdb
 import re
 
-from django.http import HttpResponse, JsonResponse
-
 import requests
-from django.shortcuts import redirect
+from django.http import HttpResponse
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
@@ -12,25 +9,67 @@ HEADERS = {
 
 
 def proxy_url(request, user_path=None):
-    print(request.path, 111)
-    print(user_path, 222)
-    if request.path.startswith('/user-site-name'):
-        url = f'https:/{user_path}'
-        fixed_headers = HEADERS
+    full_path = request.get_full_path()
+
+    # Create url for request
+    if full_path.startswith('/user-site-name'):
+        site_name = full_path.split('/')[2]
+        url_path = full_path.removeprefix('/user-site-name/')
+        url = f'https://{url_path}'
     else:
-        url = f'https://www.ukr.net/{request.path}'
-        # pdb.set_trace()
-        fixed_headers = request.headers
-        fixed_headers['Referer'] = 'https://www.ukr.net/'
+        # Try load link from referer
+        if 'referer' in request.headers:
+            site_name = request.headers.get('referer').split('user-site-name')[1].split('/')[1]
+            url_path = full_path.removeprefix('/')
+            url = f'https://{site_name}/{url_path}'
+        else:
+            raise ValueError(f"Invalid path {full_path}")
 
-    # sess = requests.Session()
-    # pdb.set_trace()
-    res = requests.get(url, headers=fixed_headers)
-    res.raise_for_status()
-    # pdb.set_trace()
+    # Handle cors
+    if request.headers.get('Sec-Fetch-Mode') == 'cors':
+        fixed_headers = dict(request.headers.items())
+        fixed_headers = {
+            k: v.replace('http://127.0.0.1:8000', f'https://{site_name}') if 'http://127.0.0.1:8000' in v else v
+            for k, v in fixed_headers.items()
+        }
+        fixed_headers = {
+            k: v.replace('http://localhost:8000', f'https://{site_name}') if 'http://localhost:8000' in v else v
+            for k, v in fixed_headers.items()
+        }
+    else:
+        fixed_headers = HEADERS
 
-    # return HttpResponse(repl_link(res.text, user_path))
-    return HttpResponse(res.text)
+    # Do request
+    res = requests.get(url, headers=fixed_headers, cookies=request.COOKIES)
+
+    # Retry if error
+    if res.status_code != 200:
+        print(url[:100], res.status_code, res.text[:200])
+        print('Trying again...', end='')
+        res = requests.get(url, headers=HEADERS, cookies=request.COOKIES)
+        if res.status_code != 200:
+            # Ignore errors
+            print('Error', url[:100], res.status_code, res.text[:200])
+            print()
+            print()
+        else:
+            print('Fixed')
+
+    # Replace urls in response
+    response = res.text
+
+    def url_repl(matchobj):
+        return f'http://localhost:8000/user-site-name/{matchobj.group(1)}/'
+
+    response = re.sub(rf"https://(\w*\.?{site_name})/", url_repl, response)
+    response = re.sub(r"href=\"/(?!/)", f'href="http://localhost:8000/user-site-name/{site_name}/', response)
+    response = re.sub(r"src=[\"\']/(?!/)", f'src="http://localhost:8000/user-site-name/{site_name}/', response)
+
+    # Add content type in response
+    content_type = res.headers.get('Content-Type')
+
+    # Return response
+    return HttpResponse(response, content_type=content_type)
 
 
 def repl_link(site, site_name):
