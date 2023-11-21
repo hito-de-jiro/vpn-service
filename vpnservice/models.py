18a45fe56d0e67ef3a8c187403fab2a3bc1cd498
hito-de-jiro <lollexuss@gmail.com>
2023-11-21 16:41:49 +0200

    fetch: create models in models.py

--- vpnservice/models.py
+++ vpnservice/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class UserSiteModel(models.Model):
+    site_name = models.CharField(max_length=45, verbose_name='Name schema')
+    site_path = models.SlugField(max_length=45, verbose_name='site_path')
+
+    class Meta:
+        verbose_name = "user_site"
+        verbose_name_plural = "user_sites"
+
+    def __str__(self):
+        return self.site_name
+
+
+class SiteInfoModel(models.Model):
+    data_sent = models.PositiveIntegerField(verbose_name='data_sent', null=True, blank=True)
+    data_loaded = models.PositiveIntegerField(verbose_name='data_loaded', null=True, blank=True)
+    user_site = models.ForeignKey(UserSiteModel, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "site_info"
+        verbose_name_plural = "site_info"
