14376c752182b3b84cfa041504be6c5aabc7a91e
hito-de-jiro <lollexuss@gmail.com>
2023-11-22 05:17:06 +0200

    fetch: added static

--- app/urls.py
+++ app/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 user_site_name = 'user_site_name'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('vpnservice.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('vpnservice.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
