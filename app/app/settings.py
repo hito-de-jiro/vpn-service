80732067d32d5ac109964861910a19472a2ab402
hito-de-jiro <lollexuss@gmail.com>
2023-12-05 14:12:15 +0200

    chore: changed floating point fields to targets and fixed small bugs

--- app/app/settings.py
+++ app/app/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'app/static'
+    BASE_DIR / 'vpnservice'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- app/vpnservice/models.py
+++ app/vpnservice/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class UserSiteModel(models.Model):
-    site_name = models.SlugField(max_length=45, verbose_name='site_name')
+    site_name = models.SlugField(max_length=45, verbose_name='site_name', unique=True)
     site_path = models.URLField(max_length=200, verbose_name='site_path', unique=True)
 
     class Meta:
@@ -14,12 +14,11 @@ class UserSiteModel(models.Model):
 
 
 class SiteInfoModel(models.Model):
-    data_sent = models.FloatField(verbose_name='data_sent', null=True, blank=True)
-    data_loaded = models.FloatField(verbose_name='data_loaded', null=True, blank=True)
+    data_sent = models.IntegerField(verbose_name='data_sent', null=True, blank=True)
+    data_loaded = models.IntegerField(verbose_name='data_loaded', null=True, blank=True)
     number_visits = models.PositiveIntegerField(verbose_name='number_visits', default=0, null=True, blank=True)
     user_site = models.ForeignKey(UserSiteModel, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = "site_info"
         verbose_name_plural = "site_info"
-
